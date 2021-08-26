197a840fbce5e6d6765ad2993ef52974ae9d252c
AlexMuia31 <alexmuia31@gmail.com>
2021-08-26 21:07:32 +0300

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,25 @@
+import { Box, Flex, Heading, Text, useMediaQuery } from '@chakra-ui/react'
 import React from 'react'
 
 function Profile() {
+    const [isNotSmallerScreen] = useMediaQuery('(min-width:600px)');
     return (
-        <div>
-
-        </div>
+        <Flex direction={isNotSmallerScreen ? 'row' : 'column'} w='100%'
+            maxWidth={{ base: '100vh', md: '130vh', lg: '130vh', xl: '130vh' }}>
+            <Box alignSelf="center" px='10' py='16'>
+                <Heading fontWeight='extrabold' color='cyan.500' size='4xl'>
+                    3+
+                </Heading>
+                <Text fontSize='2xl' color='gray.400'>
+                    Years of Experience
+                </Text>
+            </Box>
+            <Box alignSelf='center' px='' py=''>
+                <Text fontWeight='bold' fontSize='2xl'>
+                    Web Designer and Developer. Specialised in both front-end and back-end.
+                </Text>
+            </Box>
+        </Flex>
     )
 }
 
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,10 +1,15 @@
+import { HStack, IconButton } from '@chakra-ui/react'
 import React from 'react'
+import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 function Social() {
     return (
-        <div>
+        <HStack>
+            <IconButton icon={<FaTwitter />} isRound='true' />
+            <IconButton ml={8} icon={<FaLinkedin />} isRound='true' />
+            <IconButton ml={8} icon={<FaGithub />} isRound='true' />
 
-        </div>
+        </HStack>
     )
 }
 
